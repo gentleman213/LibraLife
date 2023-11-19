05fcf19efaeaac61f4dca8ede4ad71ff2b264c52
gentleman213 <100601616+gentleman213@users.noreply.github.com>
2023-11-18 19:28:32 -0800

    Initial commit

---
+++ LibraLife/LibraLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraLifeApp.swift
+//  LibraLife
+//
+//  Created by Thibaud Barberon on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LibraLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
