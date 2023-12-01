d4e02404c3e74a1661cb414814acc4a4908b1573
Will <li.will.thomas@gmail.com>
2023-12-01 15:23:44 -0800

    implemented scrolling bookshelf code

--- LibraLife/View/ShelvesView.swift
+++ LibraLife/View/ShelvesView.swift
@@ -7,11 +7,50 @@
 
 
 import SwiftUI
+import SwiftData
 
 struct ShelvesView: View {
+    @Query private var allBookShelf: [Shelf]
+
+    let rows = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
         VStack {
+            Text("Bookshelf Display").font(.system(size: 32, weight: .bold))
+        ZStack {
+            Image("shelf")
+                .resizable()
+                .scaledToFill()
+            
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: rows, alignment: .bottom, spacing: 70) {
+                        ForEach(0 ..< allBookShelf.count, id: \.self) { book in
+                            ShelfBookView(bookShelf: allBookShelf[book])
+                        }
+                    }.frame(width:300, height: 590)
+                }
+            }
 
         }
     }
+}
+
+struct ShelfBookView: View {
+    let bookShelf: Shelf
+    var body: some View {
+        Image(bookShelf.title)
+            .resizable()
+            .frame(width: 60, height: 95)
+            .background(.gray)
+    }
+}
+
+#Preview {
+    ShelvesView()
 }
